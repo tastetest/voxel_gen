f5fcfe0168f2027542dc9fdf4b1ca7b992efad3e
ttj <103539522+tastetest@users.noreply.github.com>
2022-09-26 17:38:44 -0600

    Revert "Voxel impl"

--- src/voxels.rs
+++ src/voxels.rs
@@ -1,25 +1,19 @@
-pub mod voxels {
-    use bevy::prelude::*;
+use bevy::prelude::*;
 
-    pub enum VoxelType {
-        Air,
-        Dirt,
-        Stone,
-    }
+enum VoxelType {
+    Air(f32),
+    Stone(f32),
+    Dirt(f32)
+}
+#[derive(Component)]
+struct Voxel {
+    Vec3: (f32, f32, f32),
+    id: f32,
+    block_type: f32,
+}
+// going to generate a chunk
+fn chunk_gen(mut commands: Commands) {
 
-    #[derive(Copy, Clone, Debug)]
-    pub struct Voxel {
-        color: Vec3,
-        block_id: u8,
-    }
+    let voxel_gen = Vec3::new(0.2, 3.0, 4.0);
 
-
-    impl Voxel {
-        fn voxel_generate(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
-            commands.spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })), 
-                ..default()
-            });
-        }
-    }
 }
